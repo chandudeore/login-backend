d7d96514d3e156420ee4ae19018f788e779a7a0d
chandudeore <chandudeore2016@gmail.com>
2022-05-20 18:20:08 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-const express = require('express');
-const connect = require('./config/db');
+const express = require("express");
+const connect = require("./config/db");
+require("dotenv").config();
 
-const userController = require('./controllers/user.controller');
-const productController = require('./controllers/product.controller');
+const userController = require("./controllers/user.controller");
+const productController = require("./controllers/product.controller");
 
-const { register, login } = require('./controllers/auth.controller');
+const { register, login } = require("./controllers/auth.controller");
 const app = express();
 
 app.use(express.json());
 
-
-
-app.post('/register', register);
-app.post('/login', login);
+app.post("/register", register);
+app.post("/login", login);
 app.use("/users", userController);
 app.use("/products", productController);
 
-app.listen(9999, async () => {
-    try {
-        await connect();
-            console.log("listening on port 9999");
-    }
-    catch (e) {
-        console.error(e.message);
-   }
-})
+app.listen(process.env.PORT || 9999, async () => {
+  try {
+    await connect();
+    console.log("listening on port 9999");
+  } catch (e) {
+    console.error(e.message);
+  }
+});
