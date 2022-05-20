2ba7592654dbf36aa65527b900080363fceeeb19
chandudeore <chandudeore2016@gmail.com>
2022-05-20 20:22:48 +0530

    chad

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 module.exports = () => {
-    return mongoose.connect('mongodb://127.0.0.1:27017/authentication');
-}
+  return mongoose.connect(process.env.MONGOURL);
+};
